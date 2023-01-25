4dca4daefec8c28b495aedd09bc75a755b66ff1b
José Carlos Méndez Gómez <josecarlosmendezgomez@outlook.com>
2023-01-25 12:38:48 -0600

    Add project files.

---
+++ DemoMigration/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoMigration;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DemoMigration/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoMigration;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
